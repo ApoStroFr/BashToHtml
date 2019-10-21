d1713b8b09727a907159e66d39ad50ce94ae4de0
ApoStroFr <35300351+ApoStroFr@users.noreply.github.com>
2019-10-21 16:18:37 +0200

    Update BashToHtml.sh

--- BashToHtml.sh
+++ BashToHtml.sh
@@ -5,3 +5,31 @@ then
   echo -e "Error: Fichier à copier n'existe pas\n\n Fichier à copier n'existe pas"
   exit 0
 fi
+
+#Cas ou output existe déjà
+if [ -f "$2" ]
+then
+  echo -e "Error Fichier déjà existant \n\n Veuillez chhoisir un autre fichier sortant"
+  exit 0
+fi
+
+bash=0
+while read -r line
+do
+  if [[ $line = "<!--" ]]
+  then
+    let "bash=1"
+    continue
+  elif [[ $line = '-->' ]]
+  then
+    let "bash=0"
+    continue
+  fi
+  
+  if [[ $bash = 1 ]]
+  then
+    echo "$line" >> $2
+  else
+    echo "echo -e \"$line\"" >> $2
+  fi
+done < "$1"
